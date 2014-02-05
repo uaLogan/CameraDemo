2bacc308ebdf965d5d35038c7655781d56318872
uaLogan <lglin@cs.ualberta.ca>
2014-02-04 19:11:26 -0700

    It's working

--- src/ualberta/cmput301/camerademo/CameraDemoActivity.java
+++ src/ualberta/cmput301/camerademo/CameraDemoActivity.java
@@ -1,9 +1,16 @@
 package ualberta.cmput301.camerademo;
 
+import java.io.File;
+
 import ualberta.cmput301.camerodemo.R;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,7 +18,9 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class CameraDemoActivity extends Activity {
-
+	private final File FOLDER_NAME = new File(Environment.getExternalStorageDirectory()+"/tmp/");
+	private final File FILE_NAME = new File(FOLDER_NAME, "hi.jpg");
+	
 	private TextView textView;
 	private ImageButton imageButton;
 	private Uri imageFileUri;
@@ -19,6 +28,9 @@ public class CameraDemoActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camero_demo);
+		
+		if(!FOLDER_NAME.exists())
+			FOLDER_NAME.mkdir();
 		
 		// Retrieve handlers
 		textView = (TextView) findViewById(R.id.status);
@@ -34,17 +46,39 @@ public class CameraDemoActivity extends Activity {
 		imageButton.setOnClickListener(listener);
 	}
 
-
 	// Implement takeAPhoto() method to allow you to take a photo when you click the ImageButton.
 	// Notice that startActivity() method will not return any result when the launched activity 
 	// finishes, while startActivityForResult() method will. To retrieve the returned result, you may 
 	// need implement onAcitityResult() method.
 	public void takeAPhoto() {
-		// To Do		
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		
+		Uri uri = Uri.fromFile(FILE_NAME);
+		
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+		
+		startActivityForResult(intent, 0);
 	}
 	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// To Do
+		if(resultCode == RESULT_OK)
+		{
+			//Bitmap bm = (Bitmap)data.getExtras().getParcelable("data");
+			//imageButton.setImageBitmap(bm);
+			
+			Uri uri = Uri.fromFile(FILE_NAME);
+			imageButton.setImageDrawable(Drawable.createFromPath(uri.getPath()));
+			
+			textView.setText("Photo taken");
+		}
+		else if(resultCode == RESULT_CANCELED)
+		{
+			textView.setText("Photo received but also canceled??");
+		}
+		else
+		{
+			textView.setText("Unknown error");
+		}
 	}	
 	
 	@Override
